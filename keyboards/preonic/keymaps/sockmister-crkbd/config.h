116724343a850d34da1797d6588546fc07c46a22
poh <pohchiat@gmail.com>
2023-02-26 11:06:04 +0100

    add layer taps for raise and lower

--- keyboards/preonic/keymaps/sockmister-crkbd/config.h
+++ keyboards/preonic/keymaps/sockmister-crkbd/config.h
@@ -32,3 +32,6 @@
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 200
--- keyboards/preonic/keymaps/sockmister-crkbd/keymap.c
+++ keyboards/preonic/keymaps/sockmister-crkbd/keymap.c
@@ -33,6 +33,10 @@ enum preonic_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+// define layer taps
+#define LWR_SPC LT(_LOWER, KC_SPC)
+#define RAS_ESC LT(_RAISE, KC_ESC)
+
 /* Qwerty
  */
 [_QWERTY] = LAYOUT_preonic_grid(
@@ -40,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  _______, _______, _______, KC_LGUI, LOWER,   KC_ESC,  KC_SPC,  RAISE,   KC_RALT, _______, _______, _______
+  _______, _______, _______, KC_LGUI, LOWER,   RAS_ESC,  LWR_SPC,  RAISE,   KC_RALT, _______, _______, _______
 ),
 
 /* Lower
