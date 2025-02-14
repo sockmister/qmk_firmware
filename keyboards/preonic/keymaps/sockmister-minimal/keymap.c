9904390a6d17d1565791e988b562fd9b1958b913
poh <pohchiat@gmail.com>
2025-02-14 17:33:27 +0100

    add right ctrl for tesmart hotkey

--- keyboards/preonic/keymaps/sockmister-minimal/keymap.c
+++ keyboards/preonic/keymaps/sockmister-minimal/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  _______, _______, _______, KC_LGUI, LOWER,   KC_LALT, KC_SPC,  RAISE,   NAV, _______, _______, _______
+  _______, _______, _______, KC_LGUI, LOWER,   KC_LALT, KC_SPC,  RAISE,   NAV,     KC_RCTL, _______, _______
 ),
 
 /* Lower
@@ -70,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |      | Alt  |      |      |      |      |      | Mute | VolDn| VolUp| Play |
  * `-----------------------------------------------------------------------------------'
- */ 
+ */
 [_LOWER] = LAYOUT_preonic_grid(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, KC_UP, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______,
-  KC_ESC, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______,
+  KC_ESC,  _______, _______,_______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,
   KC_LSFT, _______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,  KC_BSLS, _______,
   KC_LCTL, _______, KC_LALT, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
 ),
